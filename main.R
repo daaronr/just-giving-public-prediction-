db437e9dddb99c70cad0626ef7a2cc1cee5d4c51
David Reinstein <daaronr@gmail.com>
2019-11-26 17:36:32 +0000

    clean and knit prediction markdown

--- main.R
+++ main.R
@@ -10,10 +10,9 @@ library(pacman)
 
 here <- here::here
 
-p_load(dplyr,magrittr,purrr,tidyverse,tidyr,broom,janitor,here,glue,dataMaid,glue,readr, lubridate,summarytools,gtools,knitr,pastecs,data.table, kableExtra)  #citr, reporttools, experiment, estimatr,  kableExtra, ggsignif, glmnet, glmnetUtils, rsample,snakecase,zoo
+p_load(dplyr,magrittr,purrr,tidyverse,tidyr,broom,janitor,here,glue,dataMaid,glue,readr, lubridate,summarytools,gtools,knitr,pastecs,data.table, kableExtra, recipes)  #citr, reporttools, experiment, estimatr,  kableExtra, ggsignif, glmnet, glmnetUtils, rsample,snakecase,zoo
 
 library(codebook)
-
 
 source(here("R","baseopt_jg.R")) #basic options, definitions, abbreviations for functions, folder path names
 
@@ -46,3 +45,7 @@ rdr_cbk("codebook_uc_fdd.Rmd") #codebook for donation data (1 per don) with fund
 
 rdr_cbk("codebook_uc_fdd_fd.Rmd") #codebook for data aggregating donations at fundraiser level   - UK fundraisers with 1+ donation, plausibly completed ()25 weeks rule
 
+####################
+# Render html file for prediction assignment and discussion 
+
+rmarkdown::render(here("prediction", "prediction.Rmd"), output_dir=here("prediction"), intermediates_dir = here("prediction"), knit_root_dir=here("prediction"), output_format = "html_document")
