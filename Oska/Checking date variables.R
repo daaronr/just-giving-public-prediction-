2dea90850426eb52ecf064015355881e95341f68
Oska Fentem <os301@exeter.ac.uk>
2020-02-01 17:08:07 +0000

    starting to check date variables

---
+++ Oska/Checking date variables.R
@@ -0,0 +1,17 @@
+#Checking to what the variables dur_cdate and dur_edate are.
+#The date which the API pulled the data/data is from??
+2018-04-15 15:58:14
+
+#Created date: 
+cr_d <- as.Date(2019-01-04 21:32:07)
+
+#dur_edate:
+4.8437
+
+#dur_cdate:
+1.9464
+
+#Event date
+ev_d <- 2019-01-02 00:00:00
+
+difftime()
