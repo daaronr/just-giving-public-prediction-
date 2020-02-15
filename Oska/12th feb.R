a08a5fa96e62e769531b7bab955992686e0255a3
Oska Fentem <os301@exeter.ac.uk>
2020-02-15 19:37:44 +0000

    15th feb

--- Oska/12th feb.R
+++ Oska/12th feb.R
@@ -10,6 +10,19 @@ uc_fdd_fd %>%
     activity_id = "An identifier number for the fundraising activity.",
     event_id = "An identifier number for the fundraising event.",
     activity_type = "The type of fundraising event, for example a Charity Cycle.",
+    event_date = "Date on which the fundraising event occurred or will occur.",
+    expiry_date = "The date when fundraising ended, in this case all expiry dates are NA.",
+    Owner = "The owner of the fundraiser (the person/organisation which set up the fundraiser).",
+    Status = "Whether the fundraiser was Active, Cancelled or Completed at the time in which the API pulled the data.",
+    created_date = "Date when the fundraiser was created.",
+    wday_created = "The day of the week on which the event was created. This might be a useful dummy variable.",
+    hr_created = "The hour of the day in which the fundraiser was created.",
+    count_don = "The number of donations.",
+    sum_don = "The sum of donations of this fundraiser.",
+    med_don = "The median donation of this fundraiser.",
+    mn_don = "The mean donation of this fundraiser.",
+    dur_cdate = "This is defined as being the interval between the created date and the variable donationDate.",
+    dur_edate = "This is defined as being the interval between the EventDate and the donationDate."
     ) %>%
   get_label()
 
@@ -19,6 +32,3 @@ writeLines(tx2, con="C:/Users/oskas/OneDrive/Desktop/work.txt")
 
 uc_fdd_fd <- uc_fdd_fd %>% 
   rename(uc_fdd_fd, colnames(uc_fdd_fd) = (to_snake_case(colnames(uc_fdd_fd))))
-
-
-                  
